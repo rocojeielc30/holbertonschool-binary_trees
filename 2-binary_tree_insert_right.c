6f297c961742b9c302a8968a1ac63d56d1cf0760
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-08-19 14:44:40 +1000

    fixed logic / syntax error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 	{
 		return (NULL);
