35b39d6e4a16144c6fae667280444731c3ca004d
Jeiel Roco <jeiel@Jeiels-MacBook-Pro.local>
2026-08-23 23:57:12 +1000

    fix logic

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,25 @@
 #include "binary_trees.h"
 
 /**
+ * height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: height of the tree, or 0 if tree is NULL
+ */
+static size_t height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = tree->left ? 1 + height(tree->left) : 0;
+	right_height = tree->right ? 1 + height(tree->right) : 0;
+
+	return (left_height > right_height ? left_height : right_height);
+}
+
+/**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance
  * factor
@@ -10,9 +29,7 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	return (binary_tree_height(tree->left) -
-		binary_tree_height(tree->right));
+
+	return ((int)height(tree->left) - (int)height(tree->right));
 }
